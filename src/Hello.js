0981aa63494f0f8f3d29b18ee141049adb391e25
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2025-04-07 21:29:41 -1000

    change test

--- src/Hello.js
+++ src/Hello.js
@@ -5,7 +5,7 @@ class Hello extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1>Hello World!</h1>
                 <p>Welcome to React</p>
             </div>
 
